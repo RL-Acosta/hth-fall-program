500869bfd5365f5033a75dde4b541e632c7ad1b0
RL-Acosta <RL-Acosta@users.noreply.github.com>
2022-10-06 18:42:11 -0700

    Completing first python activity file

---
+++ Getting-Started-with-Python-RL-Acosta/About_me.py
@@ -0,0 +1,9 @@
+name = "Rahima Lynne Acosta"
+Fav_nickname = "Lynne"
+age = 16
+Has_used_python = True
+Hobbies = ["Playing the piano", "Archery", "Playing video games"]
+My_favorites = {
+  "Color": "Yellow", "Movie": "Jumanji", "Food": "Sushi", "Anime": "Naruto", "Number": 5}
+
+all_vars = dict(vars())
